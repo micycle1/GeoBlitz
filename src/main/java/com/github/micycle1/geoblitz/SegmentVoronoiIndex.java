13af489507b6e81d42a5f1da54880ea603defff2
Michael Carleton <micycle1@hotmail.co.uk>
2026-01-29 21:26:59 +0000

    no message

--- src/main/java/com/github/micycle1/geoblitz/SegmentVoronoiIndex.java
+++ src/main/java/com/github/micycle1/geoblitz/SegmentVoronoiIndex.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.locationtech.jts.algorithm.Distance;
@@ -276,7 +277,7 @@ public class SegmentVoronoiIndex {
 			}
 
 			return results.stream();
-		}).toList();
+		}).collect(Collectors.toList());
 
 		cellBlobs.forEach(cb -> cellIndex.insert(cb.e, cb));
 	}
